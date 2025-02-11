43fa917549ebe4bce785bb3f5fb862077438f770
TheGreatMegalodon <69216391+TheGreatMegalodon@users.noreply.github.com>
2025-02-11 16:46:57 +0100

    Update juggernaut-Mod-(auto start).js

--- juggernaut-Mod-(auto start).js
+++ juggernaut-Mod-(auto start).js
@@ -1,4 +1,4 @@
-// Starblast Juggernaut v0.7
+// Starblast Juggernaut v0.8
 
 // fly
 var Fly_101 = '{"name":"Fly","level":1,"model":1,"size":1.05,"next":[],"specs":{"shield":{"capacity":[75,100],"reload":[2,3]},"generator":{"capacity":[40,60],"reload":[10,15]},"ship":{"mass":60,"speed":[175,175],"rotation":[110,130],"acceleration":[100,120]}},"bodies":{"main":{"section_segments":12,"offset":{"x":0,"y":0,"z":10},"position":{"x":[0,0,0,0,0,0,0,0,0,0],"y":[-65,-60,-50,-20,10,30,55,75,60],"z":[0,0,0,0,0,0,0,0,0]},"width":[0,8,10,30,25,30,18,15,0],"height":[0,6,8,12,20,20,18,15,0],"propeller":true,"texture":[4,63,10,1,1,1,12,17]},"cockpit":{"section_segments":12,"offset":{"x":0,"y":0,"z":20},"position":{"x":[0,0,0,0,0,0,0],"y":[-15,0,20,30,60],"z":[0,0,0,0,0]},"width":[0,13,17,10,5],"height":[0,18,25,18,5],"propeller":false,"texture":[7,9,9,4,4]},"cannon":{"section_segments":6,"offset":{"x":0,"y":-15,"z":-10},"position":{"x":[0,0,0,0,0,0],"y":[-40,-50,-20,0,20,30],"z":[0,0,0,0,0,20]},"width":[0,5,8,11,7,0],"height":[0,5,8,11,10,0],"angle":0,"laser":{"damage":[5,6],"rate":4,"type":1,"speed":[160,180],"number":1,"error":2.5},"propeller":false,"texture":[3,3,10,3]}},"wings":{"main":{"length":[60,20],"width":[100,50,40],"angle":[-10,10],"position":[0,20,10],"doubleside":true,"offset":{"x":0,"y":10,"z":5},"bump":{"position":30,"size":20},"texture":[11,63]}},"typespec":{"name":"Fly","level":1,"model":1,"code":101,"next":[],"specs":{"shield":{"capacity":[75,100],"reload":[2,3]},"generator":{"capacity":[40,60],"reload":[10,15]},"ship":{"mass":60,"speed":[175,175],"rotation":[110,130],"acceleration":[100,120]}},"shape":[1.368,1.368,1.093,0.965,0.883,0.827,0.791,0.767,0.758,0.777,0.847,0.951,1.092,1.667,1.707,1.776,1.856,1.827,1.744,1.687,1.525,1.415,1.335,1.606,1.603,1.578,1.603,1.606,1.335,1.415,1.525,1.687,1.744,1.827,1.856,1.776,1.707,1.667,1.654,0.951,0.847,0.777,0.758,0.767,0.791,0.827,0.883,0.965,1.093,1.368],"lasers":[{"x":0,"y":-1.365,"z":-0.21,"angle":0,"damage":[5,6],"rate":4,"type":1,"speed":[160,180],"number":1,"spread":0,"error":2.5,"recoil":0}],"radius":1.856,"next":[]}}';
@@ -30,10 +30,11 @@ const gameOptions = {
   decaySpeed: 2,
   
   info: {
-    name: "Juggernaut",
-    version: "0.7\n",
-    creator: "Thuliux",
-    developer: "Megalodon\n",
+    "name": "Juggernaut",
+    "version": "0.8\n",
+    "creator": "Thuliux",
+    "developer": "Megalodon",
+    "coding help": "Julia\n",
     
     "flagShip Builders": "Nok, Kuroyammy",
     "Ship Builders": "Thuliux, Megalodon, Basit, ShadowFighter191, A198\n"
@@ -86,8 +87,6 @@ const gameOptions = {
     }
   }
 };
-
-
 
 const vocabulary = [
   { text: "You", icon:"\u004e", key:"O" },
@@ -389,7 +388,7 @@ var functions = {
         ]
       });
     },
-    findColor: function(color) { // do colors
+    findColor: function(color) {
       switch(color) {
         case "none": return;
         case "green": return "#70ffc1";
@@ -602,14 +601,12 @@ var functions = {
   endGame: {
     jugg: function(game) { // jugg lost
       let jugg = game.custom.memory.isJuggernaut;
+      for (let ship of game.ships) ship.set({collider: false});
       
       functions.usage.alert(jugg, `Juggernaut Terminated!`, `Human battalion has destroyed your ship!`, ``, `red`, 8000, {v1: 6, v2: 4, v3: 4});
       game.ships.filter(ship => ship !== jugg).forEach(ship => {
         functions.usage.alert(ship, `Mission success!`, `The Juggernaut has been eliminated!`, ``, `yellow`, 8000, {v1: 6, v2: 4, v3: 4});
       });
-      
-      for (let ship of game.ships) ship.set({collider: false});
-
       functions.stepTimeout.seted(() => {
         for (let ship of game.ships) {
           ship.gameover({
@@ -621,14 +618,12 @@ var functions = {
     },
     players: function(game) { // players lost
       let jugg = game.custom.memory.isJuggernaut;
+      for (let ship of game.ships) ship.set({collider: false});
       
       functions.usage.alert(jugg, `Control Stations secured!`, `Human battle stations have been suppressed!`, ``, `yellow`, 8000, {v1: 6, v2: 4, v3: 4});
       game.ships.filter(ship => ship !== jugg).forEach(ship => {
         functions.usage.alert(ship, `Mission failed!`, `The Juggernaut has captured the Control Stations!`, ``, `red`, 8000, {v1: 6, v2: 4, v3: 4});
       });
-      
-      for (let ship of game.ships) ship.set({collider: false});
-
       functions.stepTimeout.seted(() => {
         for (let ship of game.ships) {
           ship.gameover({
@@ -637,7 +632,6 @@ var functions = {
           });
         }
       }, 10000);
-      
     }
   }
 };
@@ -978,7 +972,7 @@ function prepareGame() {
   
   try {
     gameCommands.resolveCommands();
-    gameCommands.log('\n\n  Custom Commands installed', '');
+    gameCommands.log('\n  Custom Commands installed', '');
     game.custom.commandsOn = true;
     functions.stepTimeout.seted(() => {
       echo(`\n[[ig;#85ff70;]Write] [[g;Gold;]<info commands>] [[ig;#85ff70;]in the console]\n[[ig;Cyan;]For more information on the mod and its integrated commands.]\n`)
@@ -995,8 +989,6 @@ function prepareGame() {
       const formattedKey = `${key.charAt(0).toUpperCase() + key.substring(1)}:`.padEnd(maxLength + 2);
       echo(`[[i;Cyan;]     ${formattedKey}] [[i;Gold;]${gameOptions.info[key]}]`);
     });
-    
-      // do tests
     echo(`\n[[i;#ffb670;]  Procecutive Order % Starting]`);
   }
   
